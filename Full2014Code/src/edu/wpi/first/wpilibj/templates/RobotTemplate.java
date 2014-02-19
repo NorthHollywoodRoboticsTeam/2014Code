e44d347a614faa4a5562195d5434f402d6434da2
Henry Birge-Lee <henrybirgelee@gmail.com>
2014-02-19 00:14:41 -0800

    Final pre-competiton code with debug statement (might have huge feeder logic error)

--- Full2014Code/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Full2014Code/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -6,12 +6,15 @@
 /*----------------------------------------------------------------------------*/
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,13 +29,21 @@ public class RobotTemplate extends SimpleRobot {
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-
+        /*try {
+             if (((Double)(server.getValue("BLOB_COUNT"))).intValue() >= 2) {
+             System.out.println("shoot NOW");
+             }else{ 
+             System.out.println("Don't shoot");
+             }
+             } catch (Exception ex) {
+                
+             }*/
     }
 
     /*
      Joystick spec.
      Driving: js1 Y for forward and backward, js1 X for turn and js2 X for sideways
-     Winching: js3 up and down on knob
+     Winching: js3 up and down on knob   and js3 button 4 is auto winch up and js3 button 3 is autowinch down.
      Feeding: Toggle js2 10 for on and off and js2 11 overrides that and dose reverse
      Shooting: js3 button 2 turns it on and js3 trigger resets it
      */
@@ -45,36 +56,87 @@ public class RobotTemplate extends SimpleRobot {
      */
     RobotDrive drive = new RobotDrive(new Jaguar(2), new Jaguar(1), new InvertedSpeedControler(new Jaguar(3)), new InvertedSpeedControler(new Jaguar(4)));
 
-    Jaguar winch1 = new Jaguar(5), winch2 = new Jaguar(6);
+    
+    DigitalInput limitSwitch = new DigitalInput(1);
+    Victor winch1 = new Victor(5);
+    Jaguar winch2 = new Jaguar(6);
     Relay feeder1 = new Relay(2), feeder2 = new Relay(3);
     Relay electroMagnet = new Relay(1);
     Joystick js1 = new Joystick(1), js2 = new Joystick(2), js3 = new Joystick(3);
 
-    //js 3 ax 5  is x and 6 is Y
+    
     //NetworkTable server = NetworkTable.getTable("SmartDashboard");
+    
+    
+    //js 3 ax 5  is x on the knob and 6 is Y on the knob
+    //The limit switch is inverted
+    
     boolean button3State = false, button10State = false;
 
     boolean feederOn = false;
 
+    public final long winchingTime = (long) (3.5 * 1000);
     /**
      *
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
+        long winchingStartTime = -1;
+        
+        boolean isWinching = false;
+        
+        //true = winching down false = winching up
+        boolean autoWinchDirection = false;
         System.out.println("starting en.");
         while (isOperatorControl() && isEnabled()) {
-            /*try {
-             if (((Double)(server.getValue("BLOB_COUNT"))).intValue() >= 2) {
-             System.out.println("shoot NOW");
-             }else{ 
-             System.out.println("Don't shoot");
-             }
-             } catch (Exception ex) {
-                
-             }*/
+            System.out.println("feeder status: " + feeder1.get());
+        SmartDashboard.putBoolean("Limit Switch Status", !limitSwitch.get());
+            
+            if (js3.getRawButton(3)) {
+                winchingStartTime = System.currentTimeMillis();
+                isWinching = true;
+                autoWinchDirection = true;
+                winchForward();
+            }
+            if (js3.getRawButton(4)) {
+                winchingStartTime = System.currentTimeMillis();
+                isWinching = true;
+                autoWinchDirection = false;
+                winchReverse();
+            }
+            
+            if (winchingStartTime != -1 && isWinching) {
+                if (System.currentTimeMillis() - winchingStartTime > winchingTime) {
+                    winchingStartTime = -1;
+                    isWinching = false;
+                    winchStop();
+                }
+            }
+            if (js3.getRawAxis(6) != 0) {
+                if (js3.getRawAxis(6) == -1) {
+                    winchForward();
+                } else {
+                    winchReverse();
+                }
+                winchingStartTime = -1;
+                isWinching = false;
+            } else if (!isWinching) {
+                winchStop();
+            }
+            
+            if (!limitSwitch.get() && isWinching) {
+                if (autoWinchDirection == true) {
+                    isWinching = false;
+                    winchingStartTime = -1;
+                    winchStop();
+                }
+            }
+            if (js3.getRawButton(8)) {
+                winchingStartTime = -1;
+                isWinching = false;
+                winchStop();
+            }
 
-            winch1.set(js3.getRawAxis(6));
-            winch2.set(-js3.getRawAxis(6));
             if (!js3.getRawButton(2)) {
                 button3State = false;
             }
@@ -114,11 +176,26 @@ public class RobotTemplate extends SimpleRobot {
                 }
             }
 
-            drive.mecanumDrive_Cartesian(js1.getY(), js2.getX(), js1.getX() / 2, 0);
+            drive.mecanumDrive_Cartesian(-js1.getY(), -js2.getX(), js1.getX() / 2, 0);
             //drive.mecanumDrive_Polar(js1.getMagnitude(), js1.getDirectionDegrees() - 90, js2.getX());
         }
     }
 
+    private void winchStop() {
+        winch1.set(0);
+        winch2.set(0);
+    }
+
+    private void winchForward() {
+        winch1.set(1);
+        winch2.set(-1 * .985);
+    }
+
+    private void winchReverse() {
+
+        winch1.set(-1);
+        winch2.set(1 * .985);
+    }
     /**
      * This function is called once each time the robot enters test mode.
      */
